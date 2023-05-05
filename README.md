2473a534f2b8fb96cf70f0fcdbce9ff32d8523ce
HoangNHbdaf190022 <hoangxanxan@gmail.com>
2023-05-05 17:48:59 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+### Session 6 - VIEW_ENGINE
+Trên file HTML chỉ viết đưuọc HTML chứ không thể dùng các login như vòng lặp các thứ nhưng VIEW ENGINE giúp chúng ta xử lý các điều kiện logic trong file HTML.
+Trong session này chúng ta sẽ làm việc việc với:
+    TEMPLATE_ENGINE: EJS
+    Body-parser để chuyển cấu trúc của data dưới dạng JSON
+    Nodemon để chạy node nhanh mượt mà hơn
+    babel để chuyển đổi các ES cũ để tương thích với các ES hiện tại
+
+Các cài đặt trong session:
+EJS | nodemon | body-parser | @babel/core | @babel/node | @babel/preset-env
+-- npm install --save-exact ejs@3.1.9 body-parser@1.19.0 nodemon@2.0.12 @babel/core@7.15.5 @babel/node@7.15.4 @babel/preset-env@7.15.6
+
+### Session 7 - STATIC FILES
+Người dùng muốn từ giao diện muốn lấy file ảnh, media của chúng ta!
+Chúng ta không muốn người dùng đạt được điều đó!
+=> Static Files ra đời để cho phép HOẶC chặn người dùng thực hiện điều đó.
+
+Trong session này chúng ta sẽ làm việc việc với:
+    File Môi Trường: .env
+    -- npm install --save-exact dotenv@10.0.0
+    
+
---
+++ src/server.js
@@ -0,0 +1,117 @@
+/*
+// *** Làm quen với NodeJS Platform ***
+//Điều kiện tiên quyết để tạo client và server
+const http = require('http');
+
+//khởi tạo server
+const server = http.createServer((req, res) => {
+    //mỗi lần có request thì sẽ chạy vào đây
+    console.log('Requested!');
+    //.write() để viết nội dung vào response
+    res.write('<h1>Hello World!</h1>')
+    res.write('<h2>Hello World!</h2>')
+    res.write('<h3>Hello World!</h3>')
+    res.write('<h4>Hello World!</h4>')
+    res.write('<h5>Hello World!</h5>')
+    res.write('<h6>Hello World!</h6>')
+    //.end() để kết thúc response
+    res.end("<h1>Ok nha</h1>")
+    //Sau res.end thì không gọi được res.write nữa
+    // res.write('<h1>Hello World!</h1>')
+
+})
+// bắt đầu lắng nghe request tại port 3300 (port gì cũng được miễn là số chưa được sử dụng)
+// server.listen nhận vào 3 param => listen(port, host, callback)
+server.listen(3300, 'localhost', () => {
+    console.log('Chay roi ban ei!')
+})
+*/
+
+/*
+// *** Làm quen với ExpressJS framwork ***
+const express = require('express');
+const app = express();
+const port = 3300;
+
+//http://localhost:3300/ => Home Page
+app.get('/', (req, res) => {
+    res.send("Home Page")
+})
+//http://localhost:3300/me => info Page
+app.get('/me', (req, res) => {
+    res.send("Hello, my name is Hoang")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+});
+
+ */
+
+/*
+ // *** Làm quen với VIEW ENGINE - template engines ***
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = 3300;
+
+//http://localhost:3300/ => Home Page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+//http://localhost:3300/me => info Page
+app.get('/me', (req, res) => {
+    res.send("Hello, my name is Hoang")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+});
+ */
+
+/*
+    // *** Ứng dụng View-engine / EJS ***
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+
+const app = express();
+const port = 8080;
+
+configViewEngine(app);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.get('/me', (req, res) => {
+    res.send("Hello, my name is Hoang")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+});
+*/
+
+// *** ffasdf ***
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import express from 'express'
+import configViewEngine from "./configs/viewEngine";
+
+dotenv.config()
+const app = express();
+const port = process.env.PORT || 8080;
+
+configViewEngine(app);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.get('/me', (req, res) => {
+    res.send("Hello, my name is Hoang")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+});
